9758f766933ec96b9b1775ace69d2a5544f1cfde
Didier Dubois <didier@didier-dubois.ch>
2016-10-17 18:25:42 +0200

    Add some example tips

--- example.py
+++ example.py
@@ -6,6 +6,8 @@ t0 = time.time()
 fname='primes-1-100000.txt'
 
 with open(fname) as fh:
+    # Note: in oder to have integers, use [ int(p) for p in fh ]
+    # ... but this not what you always want
     primes=[ p for p in fh]
 
 print "Loaded %d primes in %0.3fs" % (len(primes), time.time() -t0 )
