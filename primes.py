10b0a5d1bab9575a7eb7cf47f426753626731c9a
Didier Dubois <didier@didier-dubois.ch>
2016-10-18 18:49:19 +0200

    Add Generator loading file

--- primes.py
+++ primes.py
@@ -6,6 +6,18 @@ from math import sqrt
 import time
 t0 = time.time()
 
+
+def load(fname, limit):
+    """ Genearator to quiclky load the primes below a certain value
+    """
+    with open(fname) as fh:
+        for row in fh:
+            v=row.strip()
+            if int(v) < limit:
+                yield v
+            else:
+                return
+            
 def isPrime(n):
     lim = int(sqrt(n))+1
     for i in xrange(2, lim ) :
